9fafcb5d9ba57e0ea3294e8f5b18e1fabd5f7b94
Tayfun <dev.akguc@gmail.com>
2024-10-06 23:09:10 +0300

    case when userId is zero

--- better-stack-logging/src/test/java/com/spring/boot/dev/notes/service/impl/UserServiceImplTest.java
+++ better-stack-logging/src/test/java/com/spring/boot/dev/notes/service/impl/UserServiceImplTest.java
@@ -1,10 +1,16 @@
 package com.spring.boot.dev.notes.service.impl;
 
 import com.spring.boot.dev.notes.service.MockService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
@@ -15,4 +21,14 @@ public class UserServiceImplTest {
     @Mock
     private MockService mockService;
 
+    @Test
+    void getUserById_whenUserIdIsZero_thenThrowBadRequest() {
+        var userId = 0L;
+
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class,
+                () -> userService.getUserById(userId));
+
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+        verify(mockService, never()).logUserId(anyLong());
+    }
 }
