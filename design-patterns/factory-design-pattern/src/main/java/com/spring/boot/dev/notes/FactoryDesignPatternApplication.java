16d1050db8e23bdca6869f74550a51426be0f061
Tayfun Akgüç <dev.akguc@gmail.com>
2024-10-08 18:24:05 +0300

    FACTORY-DP-1000: create module (#13)
    * create main class
    
    * maven-jar-plugin configuration

---
+++ design-patterns/factory-design-pattern/src/main/java/com/spring/boot/dev/notes/FactoryDesignPatternApplication.java
@@ -0,0 +1,7 @@
+package com.spring.boot.dev.notes;
+
+public class FactoryDesignPatternApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
