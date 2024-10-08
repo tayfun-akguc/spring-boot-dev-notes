52b1aae4173b7001005b7e90d281df58c005e5d3
Tayfun <dev.akguc@gmail.com>
2024-10-08 16:28:35 +0300

    create main class

---
+++ design-patterns/strategy-design-pattern/src/main/java/com/spring/boot/dev/notes/StrategyDesignPatternApplication.java
@@ -0,0 +1,9 @@
+package com.spring.boot.dev.notes;
+
+public class StrategyDesignPatternApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
